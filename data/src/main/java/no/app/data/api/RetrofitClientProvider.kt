d9d86f8382c5b6f3cb1a3427fc01c363aee95453
Haakon Pettersen <haakon@datek.no>
2023-09-01 10:34:57 +0200

    Create client-provider with retrofit

---
+++ data/src/main/java/no/app/data/api/RetrofitClientProvider.kt
@@ -0,0 +1,32 @@
+package no.app.data.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import no.app.data.model.api.UserApiModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitClientProvider {
+    private val moshi = Moshi.Builder()
+        .add(UserApiModel::class.java)
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        )
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(httpClient)
+        .build()
+
+    val client: ApiEndpoints = retrofit.create(ApiEndpoints::class.java)
+}
