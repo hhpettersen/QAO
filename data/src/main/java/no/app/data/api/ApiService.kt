02c64988c61bbf6db764d349b8bf4e2f7e7a596a
Haakon Pettersen <haakon@datek.no>
2023-09-01 10:43:37 +0200

    Add ApiService-interface and convert current Api-service to implementation

--- data/src/main/java/no/app/data/api/ApiService.kt
+++ data/src/main/java/no/app/data/api/ApiService.kt
@@ -4,35 +4,8 @@ import no.app.data.model.api.UserApiModel
 import no.app.data.model.api.UserApiModelList
 import no.app.data.utils.ApiResult
 
-class ApiService(private val apiEndpoints: ApiEndpoints) {
+interface ApiService {
+    suspend fun getAllUsers(): ApiResult<UserApiModelList>
 
-    suspend fun getAllUsers(): ApiResult<UserApiModelList> {
-        return try {
-            val response = apiEndpoints.getUsers(30, 0)
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    ApiResult.Success(it)
-                } ?: ApiResult.Error(Exception("Response body was null"))
-            } else {
-                ApiResult.Error(Exception("Failed to get users, HTTP code: ${response.code()}"))
-            }
-        } catch (e: Exception) {
-            ApiResult.Error(e)
-        }
-    }
-
-    suspend fun getUserByUsername(username: String): ApiResult<UserApiModel> {
-        return try {
-            val response = apiEndpoints.getUserByUsername(username)
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    ApiResult.Success(it)
-                } ?: ApiResult.Error(Exception("Response body was null"))
-            } else {
-                ApiResult.Error(Exception("Failed to get user, HTTP code: ${response.code()}"))
-            }
-        } catch (e: Exception) {
-            ApiResult.Error(e)
-        }
-    }
+    suspend fun getUserByUsername(username: String): ApiResult<UserApiModel>
 }
---
+++ data/src/main/java/no/app/data/api/ApiServiceImpl.kt
@@ -0,0 +1,38 @@
+package no.app.data.api
+
+import no.app.data.model.api.UserApiModel
+import no.app.data.model.api.UserApiModelList
+import no.app.data.utils.ApiResult
+
+class ApiServiceImpl(private val apiEndpoints: ApiEndpoints) : ApiService {
+
+    override suspend fun getAllUsers(): ApiResult<UserApiModelList> {
+        return try {
+            val response = apiEndpoints.getUsers(30, 0)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    ApiResult.Success(it)
+                } ?: ApiResult.Error(Exception("Response body was null"))
+            } else {
+                ApiResult.Error(Exception("Failed to get users, HTTP code: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            ApiResult.Error(e)
+        }
+    }
+
+    override suspend fun getUserByUsername(username: String): ApiResult<UserApiModel> {
+        return try {
+            val response = apiEndpoints.getUserByUsername(username)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    ApiResult.Success(it)
+                } ?: ApiResult.Error(Exception("Response body was null"))
+            } else {
+                ApiResult.Error(Exception("Failed to get user, HTTP code: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            ApiResult.Error(e)
+        }
+    }
+}
